6dee5e6fc0d39d2df37a2622239a98a3e05d3479
mp0liiu <raian@reeshome.org>
2020-12-30 16:37:33 +0900

    勝者判定

--- src/index.js
+++ src/index.js
@@ -12,6 +12,26 @@ function Square(props) {
   );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] !== null && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 class Board extends React.Component {
 
   constructor(props) {
@@ -33,17 +53,27 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    if (squares[i] === null) {
-      squares[i] = this.state.xIsNext ? 'X' : 'O';
-      this.setState({
-        squares: squares,
-        xIsNext: !this.state.xIsNext,
-      });
+    if (squares[i] !== null || calculateWinner(squares) !== null) {
+      return;
     }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
-    const status = `Next player: ${ this.state.xIsNext ? 'X' : 'O' }`;
+    const winner = calculateWinner(this.state.squares);
+    const status = (() => {
+      if (winner !== null) {
+        return `Winner: ${ winner }`;
+      } else {
+        return `Next player: ${ this.state.xIsNext ? 'X' : 'O' }`;
+      }
+    })();
     return (
       <div>
         <div className="status">{ status }</div>
